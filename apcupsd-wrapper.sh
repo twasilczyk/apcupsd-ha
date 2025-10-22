504f18acabb2e3b25d16f6aa0e07ca32aaec5165
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-22 06:30:35 -0700

    Log v5

--- apcupsd-wrapper.sh
+++ apcupsd-wrapper.sh
@@ -2,6 +2,8 @@
 
 #syslog diag
 ls -l /dev/log || true
+ls -l /run/systemd/journal/dev-log || true
+test -S /run/systemd/journal/dev-log || echo "journald socket not present here"
 
 # Log to stdout
 touch /tmp/myfile

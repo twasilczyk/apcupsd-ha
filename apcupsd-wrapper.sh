7637b61d6e7434e2223e233823dad4b3f8a451da
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-22 06:35:31 -0700

    Log v7

--- apcupsd-wrapper.sh
+++ apcupsd-wrapper.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
-#syslog diag
-ls -l /dev/log || true
-ls -l /run/systemd/journal/dev-log || true
-test -S /run/systemd/journal/dev-log || echo "journald socket not present here"
 mkdir -p /run/systemd/journal
 touch /run/systemd/journal/dev-log
 
 # Log to stdout
-touch /tmp/myfile
-killall syslogd || true
-rm /dev/log || true
-syslogd -n -O /tmp/myfile &
-tail -f /tmp/myfile &
+syslogd -n -O - &
+#tail -f /tmp/myfile &
 
 apcupsd -b

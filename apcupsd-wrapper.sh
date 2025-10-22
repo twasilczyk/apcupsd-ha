82a8729c1f7894d0dd6301430224c14b6ed53af1
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-22 06:36:49 -0700

    Log v8

--- apcupsd-wrapper.sh
+++ apcupsd-wrapper.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-mkdir -p /run/systemd/journal
-touch /run/systemd/journal/dev-log
-
 # Log to stdout
+mkdir -p /run/systemd/journal
+#touch /run/systemd/journal/dev-log
 syslogd -n -O - &
-#tail -f /tmp/myfile &
 
 apcupsd -b

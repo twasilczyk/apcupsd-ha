6a3bf1d56d2f08c1d851a2cf2950db2370c92856
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-22 18:08:54 -0700

    config vnext

--- apcupsd-wrapper.sh
+++ apcupsd-wrapper.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/with-contenv bashio
 
-DEVICEADDR=$(bashio::config 'deviceaddr')
+export DEVICEADDR=$(bashio::config 'deviceaddr')
 if [ -z "$DEVICEADDR" ]; then
     bashio::log.error "UPS Address not configured!"
     exit 1
 fi
+export DEVICEPORT=161
 
-SNMPCOMMUNITY=$(bashio::config 'snmpcommunity')
+export SNMPCOMMUNITY=$(bashio::config 'snmpcommunity')
 if [ -z "$SNMPCOMMUNITY" ]; then
-    SNMPCOMMUNITY="public"
+    export SNMPCOMMUNITY="public"
 fi
 
 export POLLTIME=$(bashio::config 'polltime')
-
-echo "cfg start"
-echo 1 $(bashio::config 'deviceaddr')
-echo 2 $(bashio::config 'snmpcommunity')
-echo 3 $(bashio::config 'polltime')
-echo "cfg split"
-echo "1 $DEVICEADDR"
-echo "2 $SNMPCOMMUNITY"
-echo "2 $POLLTIME"
-echo "cfg end"
 
 cat /etc/apcupsd/apcupsd.conf.in
 envsubst < /etc/apcupsd/apcupsd.conf.in > /etc/apcupsd/apcupsd.conf

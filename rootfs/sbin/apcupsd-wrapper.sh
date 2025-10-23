2fd888420caf01b7e34f6192bd4ca09f2efb878a
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-22 22:16:22 -0700

    apparmor

--- rootfs/sbin/apcupsd-wrapper.sh
+++ rootfs/sbin/apcupsd-wrapper.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-echo "wrap12"
 # Log to stdout
 syslogd -n -O - &
 
-echo "wrap3"
 # Parse HA config
 export DEVICEADDR=$(bashio::config 'deviceaddr')
 if [ -z "$DEVICEADDR" ]; then
@@ -13,7 +11,6 @@ if [ -z "$DEVICEADDR" ]; then
 fi
 export DEVICEPORT=161
 
-echo "wrap4"
 export SNMPCOMMUNITY=$(bashio::config 'snmpcommunity')
 if [ -z "$SNMPCOMMUNITY" ]; then
     export SNMPCOMMUNITY="public"
@@ -21,11 +18,8 @@ fi
 
 export POLLTIME=$(bashio::config 'polltime')
 
-echo "wrap5"
 # Generate apcupsd config
 envsubst < /etc/apcupsd/apcupsd.conf.in > /etc/apcupsd/apcupsd.conf
 
-echo "wrap6"
 # Start apcupsd in foreground
 apcupsd -b
-echo "wrap7"

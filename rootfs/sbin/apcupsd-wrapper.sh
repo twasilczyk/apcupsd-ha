7f925f52cb3cf9fad0a8e2e8f55cfdc1ec3ca466
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-23 22:00:19 -0700

    apparmor

--- rootfs/sbin/apcupsd-wrapper.sh
+++ rootfs/sbin/apcupsd-wrapper.sh
@@ -4,19 +4,19 @@
 syslogd -n -O - &
 
 # Parse HA config
-#export DEVICEADDR=$(bashio::config 'deviceaddr')
-#if [ -z "$DEVICEADDR" ]; then
-#    bashio::log.error "UPS Address not configured!"
-#    exit 1
-#fi
-#export DEVICEPORT=161
+export DEVICEADDR=$(bashio::config 'deviceaddr')
+if [ -z "$DEVICEADDR" ]; then
+    bashio::log.error "UPS Address not configured!"
+    exit 1
+fi
+export DEVICEPORT=161
 
-#export SNMPCOMMUNITY=$(bashio::config 'snmpcommunity')
-#if [ -z "$SNMPCOMMUNITY" ]; then
-#    export SNMPCOMMUNITY="public"
-#fi
+export SNMPCOMMUNITY=$(bashio::config 'snmpcommunity')
+if [ -z "$SNMPCOMMUNITY" ]; then
+    export SNMPCOMMUNITY="public"
+fi
 
-#export POLLTIME=$(bashio::config 'polltime')
+export POLLTIME=$(bashio::config 'polltime')
 
 # Generate apcupsd config
 #envsubst < /etc/apcupsd/apcupsd.conf.in > /etc/apcupsd/apcupsd.conf

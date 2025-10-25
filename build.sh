263bdc1ce78a35933344ef31c728e88db1f6a790
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-25 11:14:05 -0700

    image

--- build.sh
+++ build.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-docker run \
-	--rm \
-	--privileged \
-	-v ~/.docker:/root/.docker \
-	-v .:/data \
-    ghcr.io/home-assistant/amd64-builder:latest \
-		--all \
-		-t /data
-
 #docker run \
 #	--rm \
 #	--privileged \
 #	-v ~/.docker:/root/.docker \
+#	-v .:/data \
 #    ghcr.io/home-assistant/amd64-builder:latest \
 #		--all \
-#		-r https://github.com/twasilczyk/apcupsd-ha \
-#		-b main
+#		-t /data
+
+docker run \
+	--rm \
+	--privileged \
+	-v ~/.docker:/root/.docker \
+    ghcr.io/home-assistant/amd64-builder:latest \
+		--all \
+		-r https://github.com/twasilczyk/apcupsd-ha \
+        -t . \
+		-b main

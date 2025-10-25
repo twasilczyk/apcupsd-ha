ed77d271012e8a356a678c59dbb5b578eb076623
Tomasz Wasilczyk <tomasz@wasilczyk.pl>
2025-10-25 10:15:56 -0700

    image

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-LABEL org.opencontainers.image.source https://github.com/twasilczyk/apcupsd-ha
 ARG BUILD_FROM=error
 FROM $BUILD_FROM
 
+LABEL org.opencontainers.image.source https://github.com/twasilczyk/apcupsd-ha
 EXPOSE 3551/tcp
 CMD [ "/sbin/apcupsd-wrapper.sh" ]
 
--- build.sh
+++ build.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-#docker run --rm --privileged \
-#  -v ~/.docker/config.json:/root/.docker/config.json:ro \
-#  -v /var/run/docker.sock:/var/run/docker.sock:ro \
-#  -v "$PWD":/data \
-#  ghcr.io/home-assistant/amd64-builder:latest \
-#    --all \
-#    -t /data \
-#    -i ghcr.io/twasilczyk/apcupsd-ha-{arch}
-
-
 docker run \
 	--rm \
 	--privileged \
 	-v ~/.docker:/root/.docker \
+	-v .:/data \
     ghcr.io/home-assistant/amd64-builder:latest \
 		--all \
-		-t apcupsd-ha \
-		-r https://github.com/twasilczyk/apcupsd-ha \
-		-b main
+		-t /data
+
+#docker run \
+#	--rm \
+#	--privileged \
+#	-v ~/.docker:/root/.docker \
+#    ghcr.io/home-assistant/amd64-builder:latest \
+#		--all \
+#		-r https://github.com/twasilczyk/apcupsd-ha \
+#		-b main
